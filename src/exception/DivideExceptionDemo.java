e2f470d4c192456dc720ba5a236a6a4e8312957f
sanketsb18 <sanketsb.18@gmail.com>
2020-01-14 10:51:40 +0530

    Exception handling exercise

---
+++ src/exception/DivideExceptionDemo.java
@@ -0,0 +1,24 @@
+package exception;
+
+import java.util.Scanner;
+
+public class DivideExceptionDemo {
+    public static void main(String[] args) {
+        //Scanner class is wrapper class of System.in object
+        Scanner inputDevice=new Scanner(System.in);
+        System.out.print("Please enter first number (numerator): ");
+        int numerator= inputDevice.nextInt();
+        System.out.print("Please enter second number (denominator): ");
+        int denominator= inputDevice.nextInt();
+        new DivideExceptionDemo().doDivide(numerator, denominator);
+    }
+
+    private void doDivide(int a, int b) {
+        try {
+            float result = a / b;
+            System.out.println("Division result of " + a + "/" + b + "=" + result);
+        }catch (ArithmeticException e){
+            System.out.println("Exception condition program is ending.");
+        }
+    }
+}
